f133d4afb0626683a1aefd25dfff41e39435828b
penaricomex-hue <penaricomex@gmail.com>
2026-09-08 23:27:33 -0600

    Update MainActivity.java

--- android/app/android/app/src/main/android/app/src/main/java/com/mipronosticodeportivo/MainActivity.java
+++ android/app/android/app/src/main/android/app/src/main/java/com/mipronosticodeportivo/MainActivity.java
@@ -1,48 +1,22 @@
 package com.mipronosticodeportivo;
 
-import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
-import android.webkit.WebSettings;
-import android.webkit.WebView;
-import android.webkit.WebViewClient;
 
-import androidx.appcompat.app.AppCompatActivity;
+public class MainActivity extends Activity {
 
-public class MainActivity extends AppCompatActivity {
-
-    private static final String APP_URL =
-            "https://mi-pronostico-deportivo.onrender.com/";
-
-    private WebView webView;
-
-    @SuppressLint("SetJavaScriptEnabled")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        webView = new WebView(this);
-        setContentView(webView);
+        Intent intent = new Intent(
+            Intent.ACTION_VIEW,
+            Uri.parse("https://mi-pronostico-deportivo.onrender.com/")
+        );
 
-        WebView.setWebContentsDebuggingEnabled(false);
-
-        WebSettings settings = webView.getSettings();
-        settings.setJavaScriptEnabled(true);
-        settings.setDomStorageEnabled(true);
-        settings.setDatabaseEnabled(true);
-        settings.setLoadsImagesAutomatically(true);
-        settings.setJavaScriptCanOpenWindowsAutomatically(true);
-
-        webView.setWebViewClient(new WebViewClient());
-
-        webView.loadUrl(APP_URL);
-    }
-
-    @Override
-    public void onBackPressed() {
-        if (webView != null && webView.canGoBack()) {
-            webView.goBack();
-        } else {
-            super.onBackPressed();
-        }
+        startActivity(intent);
+        finish();
     }
 }
