bb5d527eaef44dad28b2e639e8b8d3ca8c2f6d80
penaricomex-hue <penaricomex@gmail.com>
2026-09-09 10:24:08 -0600

    Update MainActivity.java

--- android/app/android/app/src/main/android/app/src/main/java/com/mipronosticodeportivo/MainActivity.java
+++ android/app/android/app/src/main/android/app/src/main/java/com/mipronosticodeportivo/MainActivity.java
@@ -1,22 +1,42 @@
 package com.mipronosticodeportivo;
 
 import android.app.Activity;
-import android.content.Intent;
-import android.net.Uri;
 import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 public class MainActivity extends Activity {
+
+    private WebView webView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        Intent intent = new Intent(
-            Intent.ACTION_VIEW,
-            Uri.parse("https://mi-pronostico-deportivo.onrender.com/")
-        );
+        webView = new WebView(this);
 
-        startActivity(intent);
-        finish();
+        WebSettings settings = webView.getSettings();
+        settings.setJavaScriptEnabled(true);
+        settings.setDomStorageEnabled(true);
+        settings.setDatabaseEnabled(true);
+        settings.setLoadsImagesAutomatically(true);
+
+        webView.setWebViewClient(new WebViewClient());
+
+        setContentView(webView);
+
+        webView.loadUrl(
+            "https://mi-pronostico-deportivo.onrender.com/"
+        );
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (webView != null && webView.canGoBack()) {
+            webView.goBack();
+        } else {
+            super.onBackPressed();
+        }
     }
 }
